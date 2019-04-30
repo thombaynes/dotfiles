bb6ba5df9162e3012dabfc42741d92c69c2c64e4
Thom Baynes <thom@buyapowa.com>
2019-04-30 11:12:19 -0700

    I would rather pull is easier than push.

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -11,7 +11,7 @@ function setup
     abbr gm git merge FETCH_HEAD --ff-only
     abbr gr git rb FETCH_HEAD
     abbr ga git add
-    abbr gp git push
+    abbr gp git pull
     abbr gd git diff
     abbr gco git checkout
     abbr ll ls -lhA
