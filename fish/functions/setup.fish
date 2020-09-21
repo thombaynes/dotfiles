4b6ccb1511bf7827d5856e1e62a7833d9f11e883
Thom Baynes <thom@buyapowa.com>
2020-09-21 08:52:28 -0700

    Nice pieces added.
    Terraform abbreviations.
    Couple more file types to be opened in Intellij.
    REmoving all the quicklook casks as I never really use them, and they are not a verified developer.
    New embedded sites list of repos.

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -4,7 +4,7 @@ function setup
     for abbreviation in (abbr -l)
         abbr -e $abbreviation
     end
-    
+
     abbr ai asdf install
     abbr bs brew services
     abbr bd brew doctor
@@ -14,6 +14,8 @@ function setup
     abbr besrs bundle exec spring rails s
     abbr besrc bundle exec spring rails c
     abbr bi bundle install
+    abbr dcu docker-compose up -d
+    abbr dcd docker-compose down
     abbr ga git add
     abbr gco git checkout
     abbr gc git commit -m
@@ -32,7 +34,17 @@ function setup
     abbr mci mvn clean install
     abbr mcp mvn clean pre-integration-test
     abbr rc rails console
+    abbr rps rails parallel:spec
     abbr rs rails server
-    
+    abbr rrd rails db:drop db:create db:migrate && RAILS_ENV=test rake db:drop db:create db:structure:load && DISABLE_SPRING=1 rake parallel:prepare && rake db:seed:clioco
+    abbr ta terraform apply
+    abbr ta0 terraform apply --refresh=false
+    abbr tp terraform plan
+    abbr tp0 terraform plan --refresh=false
+    abbr tr terraform refresh
+    abbr ts terraform state
+    abbr tsl terraform state list
+    abbr tsm terraform state move
+
     fisher
 end
