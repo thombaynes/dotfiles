c4e6f9ce30c10b2a9825ee4e8e1c3954e7d3d9e0
thombaynes <thom.baynes@gmail.com>
2021-04-13 08:25:37 -0700

    Bold and italic in Iterm2

---
+++ iterm2/setup.sh
@@ -0,0 +1,6 @@
+# Run these commands to add bold and italics to the terminfo database, allowing italics
+# Test by running:
+# echo `tput sitm`italics`tput ritm`
+tic tmux-256color.terminfo.txt
+tic xterm-256color-italic.terminfo.txt
+
