13e26c6016f5e430d29e25e8b72bee6da7388bc0
thombaynes <thom.baynes@gmail.com>
2021-07-06 22:24:41 -0700

    Maybe overkill but trying waaaay more abbreviations.

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -9,35 +9,50 @@ function setup
     abbr bs brew services
     abbr bd brew doctor
     abbr be bundle exec
-    abbr ber bundle exec rspec
-    abbr besr bundle exec spring rails
-    abbr besrs bundle exec spring rails s
-    abbr besrc bundle exec spring rails c
     abbr bi bundle install
+    abbr cc bundle exec rubocop -c .rubocop-codeclimate.yml
     abbr dcu docker-compose up -d
     abbr dcd docker-compose down
     abbr ga git add
-    abbr gco git checkout
+    abbr gb git branch
+    abbr gbl git branch --list
+    abbr gbd git branch -d
     abbr gc git commit -m
+    abbr gco git checkout
+    abbr gcob git checkout -b
     abbr gd git diff
     abbr gf git fetch
     abbr gfo git fetch origin
+    abbr gh git hist
     abbr gm git merge FETCH_HEAD --ff-only
     abbr gpl git pull
     abbr gps git push
     abbr gpsf git push --force-with-lease
     abbr grb git rebase
+    abbr grba git rebase --abort
+    abbr grbc git rebase --continue
+    abbr grbi git rebase -i master
     abbr grbm git rebase master
     abbr grbom git rebase origin/master
-    abbr grsh git reset --hard
-    abbr grss git reset --soft
+    abbr grsh git reset --hard origin/(git branch --show-current)
+    abbr grss git reset --soft HEAD^
     abbr gs git status
+    abbr gsh git stash
     abbr ll ls -lhA
     abbr mci mvn clean install
     abbr mcp mvn clean pre-integration-test
-    abbr rc rails console
+    abbr rc bundle exec rails console
+    abbr rdb rails db:
+    abbr rdbm rails db:migrate
+    abbr rdbp rails db:prepare
+    abbr rdbr rails db:reset
+    abbr rmod rspec (git ls-files --modified spec)
     abbr rps rails parallel:spec
     abbr rs rails server
+    abbr rsp bundle exec rspec
+    abbr sr bundle exec spring rails
+    abbr srs bundle exec spring rails s
+    abbr src bundle exec spring rails c
     abbr ta terraform apply
     abbr ta0 terraform apply --refresh=false
     abbr tp terraform plan
