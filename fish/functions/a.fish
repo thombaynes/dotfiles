3cc7d24956a255facbc72f5854a4574b34a298aa
Thom Baynes <tbaynes@usertesting.com>
2024-04-22 15:20:22 -0700

    Haven't been using this.

--- fish/functions/a.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/fish
-# A script written by someone else at UT that only works in ZSH.
-# I have it saved as a ZSH alias. See the script in the root directory /ut_aws.sh
-function a
-   zsh -c '
-   source ~/.zshrc
-   a'
-end
